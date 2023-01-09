30c88e8269543041f6cf36d5796ff2a41bcabd45
Kyra M. Bryant <40588353+kmb5482@users.noreply.github.com>
2023-01-09 13:37:53 -0500

    Add toolbox_dependencies.m
    This script lists MATLAB toolbox dependencies required to run any MATLAB Script.

---
+++ toolbox_dependencies.m
@@ -0,0 +1,8 @@
+%%  Kyra M. Bryant, PhD | First Street Foundation
+%   January 9th, 2023
+%   This script lists MATLAB toolbox dependencies.
+%   Add path and script name to line 6.
+
+[fList,pList] = matlab.codetools.requiredFilesAndProducts('D:\MATLAB_Scripts\write_new_rainfall_boundaries_full_loop_Gaia.m');
+
+{pList.Name}
